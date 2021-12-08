f3b521565b9f3926cfd1ebed4d32bbb2b138c003
TairMazriv <80627174+TairMazriv@users.noreply.github.com>
2021-12-08 17:17:51 +0200

    Update Edge_Data.java
    implement of the interface EdgeData

--- Ex2/src/api/Edge_Data.java
+++ Ex2/src/api/Edge_Data.java
@@ -3,39 +3,50 @@ package api;
 import java.util.HashMap;
 
 public class Edge_Data implements EdgeData{
-    HashMap<String, String> E = new HashMap<String, String>();
+    public int _src;
+    public double _weight;
+    public int _dest;
+    public String _info;
+    public int _tag;
+    
+    public Edge_Data(int src, double weight, int dest){
+        this._src = src;
+        this._weight = weight;
+        this._dest = dest;
+    }
+    
     @Override
     public int getSrc() {
-        return 0;
+        return this._src;
     }
 
     @Override
     public int getDest() {
-        return 0;
+        return this._dest;
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return this._weight;
     }
 
     @Override
     public String getInfo() {
-        return null;
+        return this._info;
     }
 
     @Override
     public void setInfo(String s) {
-
+        this._info = s;
     }
 
     @Override
     public int getTag() {
-        return 0;
+        return this._tag;
     }
 
     @Override
     public void setTag(int t) {
-
+        this._tag = t;
     }
 }
