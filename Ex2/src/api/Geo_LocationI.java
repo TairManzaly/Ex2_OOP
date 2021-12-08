5896b8443b8672920c78072ae5643fbfcf3320c3
TairManzaly <73856271+TairManzaly@users.noreply.github.com>
2021-12-08 17:09:36 +0200

    Add files via upload

--- Ex2/src/api/Geo_LocationI.java
+++ Ex2/src/api/Geo_LocationI.java
@@ -1,23 +1,41 @@
 package api;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+
 public class Geo_LocationI implements GeoLocation {
+    double x;
+    double y;
+    double z;
+    public Geo_LocationI(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     @Override
     public double x() {
-        return 0;
+        return this.x;
     }
 
     @Override
     public double y() {
-        return 0;
+        return this.y;
     }
 
     @Override
     public double z() {
-        return 0;
+        return this.z;
     }
 
     @Override
     public double distance(GeoLocation g) {
-        return 0;
+        //d = ((x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2)1/2
+        double x1 = Math.abs(abs(g.x()-this.x));
+        double y1 = Math.abs(abs(g.y()-this.y));
+        double z1 = Math.abs(abs(g.z()-this.z));
+
+        double d = (pow(x1,2) + pow(y1,2) + pow(z1,0.5));
+        return d;
     }
 }
--- Ex2/src/api/Node_Data.java
+++ Ex2/src/api/Node_Data.java
@@ -1,49 +1,77 @@
 package api;
 
-public class Node_Data implements NodeData{
+public class Node_Data implements NodeData {
+    int ID;
+    double x;
+    double y;
+    double z;
+    double weight;
+    int tag;
+    String data;
+
+    public Node_Data(int a, double x, double y, double z, String d,double w, int t) {
+        this.data = d;
+        this.ID = a;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.weight = w;
+        this.tag = tag;
+
+
+
+    }
 
     @Override
     public int getKey() {
-        return 0;
+        return this.ID;
     }
 
     @Override
     public GeoLocation getLocation() {
-        return null;
+        GeoLocation g = new Geo_LocationI(this.x,this.y,this.z);
+        return  g;
     }
 
     @Override
     public void setLocation(GeoLocation p) {
+        this.x = p.x();
+        this.y = p.y();
+        this.z = p.z();
+
 
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return this.weight;
     }
 
     @Override
     public void setWeight(double w) {
+        this.weight = w;
 
     }
 
     @Override
     public String getInfo() {
-        return null;
+        return this.data;
     }
 
     @Override
     public void setInfo(String s) {
+        this.data = s;
 
     }
 
     @Override
     public int getTag() {
-        return 0;
+        return this.tag;
     }
 
     @Override
     public void setTag(int t) {
+        this.tag = t;
 
     }
 }
