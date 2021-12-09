dcbd55209ee20335e5561a193a2ca1ca761d8861
TairMazriv <80627174+TairMazriv@users.noreply.github.com>
2021-12-09 02:36:05 +0200

    Add files via upload
    implements of the interface DirectedWeightedGraph

--- Ex2/src/api/Directed_Weighted_Graph.java
+++ Ex2/src/api/Directed_Weighted_Graph.java
@@ -1,26 +1,32 @@
 package api;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 
 public class Directed_Weighted_Graph implements DirectedWeightedGraph {
+    public HashMap<Integer, NodeData> _nodes;
+    public HashMap<Integer, HashMap<Integer, EdgeData>> _list;
+
     @Override
     public NodeData getNode(int key) {
-        return null;
+        return this._nodes.get(key);
     }
 
     @Override
     public EdgeData getEdge(int src, int dest) {
-        return null;
+        return this._list.get(src).get(dest);
     }
 
     @Override
     public void addNode(NodeData n) {
-
+        _nodes.put(n.getKey(), n);
     }
 
     @Override
     public void connect(int src, int dest, double w) {
-
+        EdgeData e = new Edge_Data(src, w, dest);
+        _list.get(src).put(dest, e);
     }
 
     @Override
@@ -40,17 +46,29 @@ public class Directed_Weighted_Graph implements DirectedWeightedGraph {
 
     @Override
     public NodeData removeNode(int key) {
-        return null;
+        NodeData n = _nodes.get(key);
+        if (_nodes.containsKey(key)){ _nodes.remove(key); }
+        if (_list.containsKey(key)){ _list.remove(key); }
+        Object[] keys = new Object[_list.size()];
+        keys = _list.keySet().toArray();
+        for (int i=0; i<_list.size(); i++){
+            if (_list.get(keys[i]).containsKey(key)){
+                _list.get((keys[i])).remove(key);
+            }
+        }
+        return n;
     }
 
     @Override
     public EdgeData removeEdge(int src, int dest) {
-        return null;
+        EdgeData e = _list.get(src).get(dest);
+        if (_list.get(src).containsKey(dest)){ _list.get(src).remove(dest); }
+        return e;
     }
 
     @Override
     public int nodeSize() {
-        return 0;
+        return _nodes.size();
     }
 
     @Override
