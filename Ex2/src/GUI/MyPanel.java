701a6daa8eda23cd8c2e4bc72f170674e73e40a3
TairManzaly <73856271+TairManzaly@users.noreply.github.com>
2021-12-22 23:00:43 +0200

    Add files via upload

--- Ex2/src/GUI/MyPanel.java
+++ Ex2/src/GUI/MyPanel.java
@@ -1,7 +1,6 @@
 package GUI;
 
-import api.DirectedWeightedGraph;
-import api.Directed_Weighted_Graph;
+import api.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,22 +8,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
-
+import java.util.Random;
+import java.util.prefs.NodeChangeEvent;
+import java.util.List;
+import java.util.*;
 public class MyPanel extends JPanel implements MouseListener
 {
-    private DirectedWeightedGraph graph;
+    private DirectedWeightedGraphAlgorithms graph;
     String message;
+    ArrayList<NodeData> nodes;
+    ArrayList<EdgeData> edges;
 
 
 
-    public MyPanel(){
+    public MyPanel(DirectedWeightedGraphAlgorithms graph){
         super();
-        this.setBackground(new Color(7, 43, 73)); //change color of background
-        graph = new Directed_Weighted_Graph(null,null);
+        this.graph =  graph;
         this.addMouseListener(this);
-        message = "To add a Node Press on the Screen";
+        this.setBackground(Color.WHITE);
     }
 
 //    void reset(){
@@ -32,49 +38,78 @@ public class MyPanel extends JPanel implements MouseListener
 //        this.repaint();
 //    }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("mouseClicked");
 
-    }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        message = "";
-        int x = e.getX();
-        int y = e.getY();
-        Point2D p = new Point2D.Double(x,y);
-     //   points.add(p);
-        repaint();
-        System.out.println("mousePressed");
 
-    }
+
 
     @Override
-    public void mouseReleased(MouseEvent e) {
-        System.out.println("mouseReleased");
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        System.out.println("mouseEntered");
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        System.out.println("mouseExited");
-    }
-
-    @Override
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setFont(new Font("MV Boli",Font.PLAIN,25)); //set font of text
         g.setColor(Color.white);
-        g.drawString(message, 100,100);
-        //Point2D prev = null;
-     //   DirectedWeightedGraph prev = new Directed_Weighted_Graph();
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+        Iterator iNode = graph.getGraph().nodeIter();
+        Iterator iEdge = graph.getGraph().edgeIter();
+        while (iNode.hasNext()) {
+            nodes.add((NodeData) iNode.next());
+        }
+        while ((iEdge.hasNext())) {
+            edges.add((EdgeData) iEdge.next());
+        }
+
+        System.out.println(edges);
+        System.out.println(nodes);
+        g.setFont(new Font("MV", Font.PLAIN, 25));
+        g.setColor(Color.black);
+        double minX = Double.MAX_VALUE, minY = Double.MAX_VALUE;
+        double maxX = Double.MIN_VALUE, maxY = Double.MIN_VALUE;
+
+        for (int i = 0; i < nodes.size(); i++) {
+            NodeData n = (NodeData) nodes.get(i);
+            if (n.getLocation().x() > maxX) {
+                maxX = n.getLocation().x();
+            }
+            if (n.getLocation().x() < minX) {
+                minX = n.getLocation().x();
+            }
+            if (n.getLocation().y() > maxY) {
+                maxY = n.getLocation().y();
+            }
+            if (n.getLocation().y() < minY) {
+                minY = n.getLocation().y();
+            }
+        }
+        double absX = Math.abs(maxX - minX);
+        double absY = Math.abs(maxY - minY);
+        double scaleX = (getSize().getWidth() / absX) * 0.9;
+        double scaleY = (getSize().getHeight() / absY) * 0.9;
+        for (int i = 0; i < nodes.size(); i++) {
+            g.setColor(new Color(220, 234, 26));
+            NodeData n = (NodeData) nodes.get(i);
+            n.setLocation(new Geo_LocationI(((n.getLocation().x() - minX) * scaleX * 0.9 + 32), ((n.getLocation().y() - minY) * scaleY * 0.9 + 30), 0));
+        }
+            for (int i = 0; i < edges.size(); i++) {
+                EdgeData e = (EdgeData) edges.get(i);
+                NodeData dest = (NodeData) nodes.get(e.getDest());
+                NodeData src = (NodeData) nodes.get(e.getSrc());
+//            double src_x = (src.getLocation().x() * scaleX) % getSize().getWidth();
+//            double src_y = (src.getLocation().y() * scaleY) % getSize().getHeight();
+//            double dest_x = (dest.getLocation().x() * scaleX) % getSize().getWidth();
+//            double dest_y = (dest.getLocation().y() * scaleY) % getSize().getHeight();
+                double src_x = (src.getLocation().x());
+                double src_y = (src.getLocation().y());
+                double dest_x = (dest.getLocation().x());
+                double dest_y = (dest.getLocation().y());
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setStroke(new BasicStroke(3));
+                g2.setColor(Color.black);
+                g2.draw(new Line2D.Float((int) src_x, (int) src_y, (int) dest_x, (int) dest_y));
+
+            }
+
+            //Point2D prev = null;
+            //   DirectedWeightedGraph prev = new Directed_Weighted_Graph();
 
 //        for (Point2D p : points)
 //        {
@@ -94,12 +129,71 @@ public class MyPanel extends JPanel implements MouseListener
 //
 //            prev = p;
 //        }
+
+        for (int i = 0; i < nodes.size(); i++) {
+            g.setColor(Color.gray);
+            NodeData n = (NodeData) nodes.get(i);
+            g.fillOval((int) n.getLocation().x() - 7, (int) n.getLocation().y() - 7, 15, 15);
+        }
     }
 
     public void displayHelp() {
         message = "To add a Node Press on the Screen";
         repaint();
     }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(5, 6);
+    }
+    private static void createAndShowGui() {
+        List<Integer> scores = new ArrayList<>();
+        Random random = new Random();
+        int maxDataPoints = 16;
+        int maxScore = 20;
+        for (int i = 0; i < maxDataPoints; i++) {
+            scores.add(random.nextInt(maxScore));
+        }
+
+        JFrame frame = new JFrame("DrawGraph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        paintComponent(g);
+    }
 
 
+    @Override
+    public void repaint() {
+        super.repaint();
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
